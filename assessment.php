357d29ed791fee90589bc3d83bd81e70e2e549fa
Ayush Gayakwad <94968916+ayushgayakwad@users.noreply.github.com>
2025-05-31 19:32:12 +0530

    Updates

--- assessment.php
+++ assessment.php
@@ -206,7 +206,7 @@ if ($time_left <= 0) {
     </script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Electric Vehicle Assessment</title>
+    <title>Questionnaire for Student Entrepreneurs</title>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
