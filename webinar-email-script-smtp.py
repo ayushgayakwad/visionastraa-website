f0e51b37a06542c1e97286db2ea812d9687817c5
Ayush Gayakwad <94968916+ayushgayakwad@users.noreply.github.com>
2025-07-03 10:14:08 +0530

    Updates

--- webinar-email-script-smtp.py
+++ webinar-email-script-smtp.py
@@ -17,7 +17,7 @@ SMTP_PASSWORD = '1?Q#v!$adw:M'
 
 CAMPAIGN_ID = "ev_webinar_2025_07_03"
 
-EMAIL_SUBJECT = "Job Opportunities in the EV Industry Webinar - July 03, 10:30 AM (Thu)"
+EMAIL_SUBJECT = "Reminder: Job Opportunities in the EV Industry Webinar - July 03, 10:30 AM (Thu)"
 
 EMAIL_BODY_TEMPLATE = """\
 <html>
