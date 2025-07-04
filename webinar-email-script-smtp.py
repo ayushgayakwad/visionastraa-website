9573d6c15ba336b80293856ed9cf83cac8244f23
Ayush Gayakwad <94968916+ayushgayakwad@users.noreply.github.com>
2025-07-04 13:54:39 +0530

    Updates

--- webinar-email-script-smtp.py
+++ webinar-email-script-smtp.py
@@ -15,9 +15,9 @@ SMTP_PORT = 587
 SMTP_USERNAME = 'careers@visionastraa.in'
 SMTP_PASSWORD = '1?Q#v!$adw:M'
 
-CAMPAIGN_ID = "ev_webinar_2025_07_04"
+CAMPAIGN_ID = "ev_webinar_2025_07_05"
 
-EMAIL_SUBJECT = "Reminder: Job Opportunities in the EV Industry Webinar - July 04, 10:30 AM (Fri)"
+EMAIL_SUBJECT = "Job Opportunities in the EV Industry Webinar - July 05, 11:00 AM (Sat)"
 
 EMAIL_BODY_TEMPLATE = """\
 <html>
@@ -27,8 +27,8 @@ EMAIL_BODY_TEMPLATE = """\
       Sharing webinar link for <strong>"Job Opportunities in EV Industry"</strong> for <strong>2025 graduates</strong><br>
       (B.Tech & M.Tech) from <strong>EEE, ECE & Mechanical Engineering</strong> branches only.
     </p>
-    <p><strong>Date: July 04, Fri</strong></p>
-    <p><strong>Time: 10:30 AM, IST</strong></p>
+    <p><strong>Date: July 05, Sat</strong></p>
+    <p><strong>Time: 11:00 AM, IST</strong></p>
     <p>
       Webinar Link: <a href="https://visionastraa.com/track/click.php?email={email}&target={meet_url}&campaign_id={campaign_id}" 
       target="_blank" style="color:#1a73e8;">Join Webinar</a>
@@ -62,7 +62,7 @@ EMAIL_BODY_TEMPLATE = """\
 """
 
 def create_ics():
-    dt_start = datetime(2025, 7, 4, 10, 30)
+    dt_start = datetime(2025, 7, 5, 11, 00)
     dt_end = dt_start + timedelta(minutes=60)
     dtstamp = datetime.now(timezone.utc).strftime("%Y%m%dT%H%M%SZ")
     dtstart = dt_start.strftime("%Y%m%dT%H%M%S")
@@ -77,7 +77,7 @@ DTSTAMP:{dtstamp}
 DTSTART;TZID=Asia/Kolkata:{dtstart}
 DTEND;TZID=Asia/Kolkata:{dtend}
 SUMMARY:Job Opportunities in EV Industry Webinar
-UID:visionastraa-ev-webinar-20250704@visionastraa.in
+UID:visionastraa-ev-webinar-20250705@visionastraa.in
 ORGANIZER;CN=VisionAstraa Group:mailto:{SMTP_USERNAME}
 DESCRIPTION:Join the webinar on Job Opportunities in EV Industry.\\nhttps://meet.google.com/prn-gckz-eug
 LOCATION:Online (Google Meet)
