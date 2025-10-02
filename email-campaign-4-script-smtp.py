d1b982ba12e13dad96c8b7dd47616a36489d5fca
ayushgayakwad <ag.developer8@gmail.com>
2025-10-02 22:09:46 +0530

    Updates

--- email-campaign-4-script-smtp.py
+++ email-campaign-4-script-smtp.py
@@ -14,12 +14,12 @@ SMTP_PORT = 587
 
 SMTP_ACCOUNTS = [
     {
-        'username': 'careers@visionastraa.in',
-        'password': 'Z1SIOO0A9b~'
-    },
-    {
         'username': 'visionastraa@evcourse.in',
         'password': '>p>W|jv?Kg1'
+    },
+    {
+        'username': 'careers@visionastraa.in',
+        'password': 'Z1SIOO0A9b~'
     }
 ]
 
