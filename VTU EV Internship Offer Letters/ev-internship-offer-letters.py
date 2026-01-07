134a82eacec39c283acc6acbe5b43221f2120c63
ayushgayakwad <ag.developer8@gmail.com>
2026-01-07 23:50:10 +0530

    Updates

--- VTU EV Internship Offer Letters/ev-internship-offer-letters.py
+++ VTU EV Internship Offer Letters/ev-internship-offer-letters.py
@@ -17,7 +17,7 @@ import argparse
 import math
 
 # ---------------- CONFIG ----------------
-CSV_FILE_PATH = 'VTU EV Internship Offer Letters/jan_offer_released_applicants_51.csv'
+CSV_FILE_PATH = 'VTU EV Internship Offer Letters/jan_offer_released_applicants_52.csv'
 PDF_TEMPLATE_PATH = 'VTU EV Internship Offer Letters/Template.pdf'
 OUTPUT_DIRECTORY = 'VTU EV Internship Offer Letters/Generated_Offer_Letters'
 
