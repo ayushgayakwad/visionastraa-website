1e4f4e92a6e9bc37ac212d6d503ddafddc5f3c0a
ayushgayakwad <ag.developer8@gmail.com>
2026-01-10 01:03:29 +0530

    Updates

--- VTU EV Internship Offer Letters/college_notification.py
+++ VTU EV Internship Offer Letters/college_notification.py
@@ -124,9 +124,11 @@ def send_college_notification_email(sender_email, sender_password, recipient_nam
         Greetings from <a href="https://visionastraa.com/ev-projects.html">VisionAstraa EV Academy!</a>
         <br><br>
 
-        <strong style="color:#FFD700;">
-            As the internship confirmation deadline approaches for students across all branches of your esteemed institution, we are excited to open our doors and extend internship offers to those who are yet to receive any internship offer letters - offering them the chance to gain hands-on experience, real-world exposure, and a strong foundation to start their professional journey.
-        </strong>
+        <div style="background-color:#eaffd0; padding:10px 12px; border-left:5px solid #7ac943; color:#000000;">
+            <strong>
+                As the internship confirmation deadline approaches for students across all branches of your esteemed institution, we are excited to open our doors and extend internship offers to those who are yet to receive any internship offer letters—offering them the chance to gain hands-on experience, real-world exposure, and a strong foundation to start their professional journey.
+            </strong>
+        </div>
         <br><br>
 
         We are pleased to inform you that VisionAstraa EV Academy is offering internship opportunities which are registered on the <strong><a href="https://vtu.internyet.in/companies/visionastraa-ev-academy">VTU InternYet Portal</a></strong>.
@@ -204,10 +206,9 @@ def send_college_notification_email(sender_email, sender_password, recipient_nam
 
         <div style="margin:0; padding:0; line-height:1.4; color:#000000;">
             Best Regards,<br>
-            <strong style="color:#000000;">Nikhil Jain C S</strong><br>
-            <strong style="color:#000000;">Co-founder &amp; CEO</strong>
-            <strong style="color:#000000;">VisionAstraa EV Academy</strong><br>
-            <a href="https://visionastraa.com">www.visionastraa.com</a>
+            <strong style="color:#000000;">Nikhil Jain C S</strong><br><br>
+            <strong style="color:#000000;">Co-founder &amp; CEO</strong><br>
+            <strong style="color:#000000;">VisionAstraa EV Academy</strong>
         </div>
         """
 
