ff8681c50269e611499de51c19bc55827d9c556b
ayushgayakwad <ag.developer8@gmail.com>
2026-01-10 00:53:29 +0530

    Updates

--- VTU EV Internship Offer Letters/college_notification.py
+++ VTU EV Internship Offer Letters/college_notification.py
@@ -124,9 +124,9 @@ def send_college_notification_email(sender_email, sender_password, recipient_nam
         Greetings from <a href="https://visionastraa.com/ev-projects.html">VisionAstraa EV Academy!</a>
         <br><br>
 
-        <div style="background-color:#eaffd0; padding:10px 12px; border-left:5px solid #7ac943; color:#000000;">
+        <strong style="color:#FFD700;">
             As the internship confirmation deadline approaches for students across all branches of your esteemed institution, we are excited to open our doors and extend internship offers to those who are yet to receive any internship offer letters - offering them the chance to gain hands-on experience, real-world exposure, and a strong foundation to start their professional journey.
-        </div>
+        </strong>
         <br><br>
 
         We are pleased to inform you that VisionAstraa EV Academy is offering internship opportunities which are registered on the <strong><a href="https://vtu.internyet.in/companies/visionastraa-ev-academy">VTU InternYet Portal</a></strong>.
