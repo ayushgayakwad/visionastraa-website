04ba0ec2e6b25161b600a61c46e37aecfa88eb09
ayushgayakwad <ag.developer8@gmail.com>
2026-01-31 18:52:11 +0530

    Updates

--- VTU EV Internship Offer Letters/vtu_accommodation_docs_notification.py
+++ VTU EV Internship Offer Letters/vtu_accommodation_docs_notification.py
@@ -19,12 +19,12 @@ EMAIL_SUBJECT = 'Important: Documents Required for Accommodation at VTU Belagavi
 # --- HARDCODED CREDENTIALS ---
 BATCH_CREDENTIALS = {
     1: {
-        "EMAIL": "careers@visionastraa.in",
-        "PASSWORD": "Z1SIOO0A9b~"
+        "EMAIL": "visionastraa@evinternships.com",
+        "PASSWORD": "a[kE?V6lm7G="
     },
     2: {
-        "EMAIL": "visionastraa@evcourse.in",
-        "PASSWORD": ">p>W|jv?Kg1"
+        "EMAIL": "visionastraa@evinternships.in",
+        "PASSWORD": "]9jw>Upu//Y"
     }
 }
 
