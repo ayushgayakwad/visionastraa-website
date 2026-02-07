db86ab184115a3055d7189b247590dfcd466dffb
ayushgayakwad <ag.developer8@gmail.com>
2026-02-07 19:19:23 +0530

    Updates

--- email-campaign-8-script-smtp.py
+++ email-campaign-8-script-smtp.py
@@ -14,14 +14,25 @@ from email.utils import formataddr
 SMTP_SERVER = 'smtp.hostinger.com'
 SMTP_PORT = 587
 
+# SMTP_ACCOUNTS = [
+#     {
+#         'username': 'careers@visionastraa.in',
+#         'password': 'Z1SIOO0A9b~'
+#     },
+#     {
+#         'username': 'visionastraa@evcourse.in',
+#         'password': '>p>W|jv?Kg1'
+#     }
+# ]
+
 SMTP_ACCOUNTS = [
     {
-        'username': 'careers@visionastraa.in',
-        'password': 'Z1SIOO0A9b~'
+        'username': 'visionastraa@evinternships.com',
+        'password': 'a[kE?V6lm7G='
     },
     {
-        'username': 'visionastraa@evcourse.in',
-        'password': '>p>W|jv?Kg1'
+        'username': 'visionastraa@evinternships.in',
+        'password': ']9jw>Upu//Y'
     }
 ]
 
